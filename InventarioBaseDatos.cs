34a1893eaa6d8e4a1b4d86dd5ac48cc40496a3c5
albertoreyvilela <43855857+albertoreyvilela@users.noreply.github.com>
2018-10-19 12:12:59 +0200

    Add files via upload

---
+++ InventarioBaseDatos.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Inventory/List", order = 1)]
+public class InventarioBaseDatos : ScriptableObject
+{
+    [System.Serializable]
+    public struct ObjetoInventario
+    {
+        public string nombre;
+        public Sprite sprite;
+        public string health;
+        public string damage;
+        public ItemType itemType;
+    }
+    public enum ItemType
+    {
+        weapon,
+        food,
+        mis,
+        Other,
+        
+    }
+    public enum Uso
+    {
+        
+        equipable,
+        consumible
+    }
+
+    public ObjetoInventario[] baseDatos;
+
+}
